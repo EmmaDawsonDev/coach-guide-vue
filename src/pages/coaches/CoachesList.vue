107fcdd786451fe53af3c8bb8a693870ec5e0e26
Emma Dawson <emma.l.dawson@gmail.com>
2021-01-17 16:31:18 +0100

    Initial commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    FILTER
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as coach</router-link>
+    </div>
+    <ul></ul>
+    LIST OF COACHES
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/router.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesDetail from './pages/coaches/CoachesDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
+      path: '/coaches',
+      component: CoachesList,
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachesDetail,
+      children: [
+        {
+          path: 'contact',
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachesRegistration,
+    },
+    {
+      path: '/requests',
+      component: RequestReceived,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
