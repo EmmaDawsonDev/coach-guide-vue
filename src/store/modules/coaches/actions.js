6332d6354282bad325d8199e66465c880dc2db09
Emma Dawson <emma.l.dawson@gmail.com>
2021-01-20 13:22:58 +0100

    added caching

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -25,7 +25,10 @@ export default {
 
     context.commit('registerCoach', { ...coachData, id: userId });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
       `https://vue-test-project-f4a96-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -51,5 +54,6 @@ export default {
       coaches.push(coach);
     }
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimeStamp');
   }
 };
